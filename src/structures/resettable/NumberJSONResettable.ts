352cbc0a2d0c16e8b93aed35ce5167c504a4b7c7
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-03-14 16:48:42 +0800

    Don't use `SetStateAction` for `NumberResettable`

--- src/structures/resettable/NumberJSONResettable.ts
+++ src/structures/resettable/NumberJSONResettable.ts
@@ -4,4 +4,5 @@ import { NumberResettable } from "./NumberResettable";
 /**
  * A storage structure that allows the stored number to be reset and saved to a skin.json.
  */
-export type NumberJSONResettable = NumberResettable & JSONResettable<number>;
+export type NumberJSONResettable = NumberResettable &
+    Omit<JSONResettable<number>, "setValue">;
--- src/structures/resettable/NumberResettable.ts
+++ src/structures/resettable/NumberResettable.ts
@@ -3,7 +3,14 @@ import { Resettable } from "./Resettable";
 /**
  * A number `Resettable` that has additional properties to deal with numbers.
  */
-export interface NumberResettable extends Resettable<number> {
+export interface NumberResettable extends Omit<Resettable<number>, "setValue"> {
+    /**
+     * Sets the value of this `NumberResettable`.
+     *
+     * @param value The new value. Defaults to the default value.
+     */
+    setValue(value?: number): void;
+
     /**
      * The minimum number allowed.
      */

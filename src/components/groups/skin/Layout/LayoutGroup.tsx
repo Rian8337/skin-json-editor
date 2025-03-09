c3c69f9fab5b80d0992f0fef0b791c860b8850cb
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-03-10 06:15:45 +0700

    Put song selection menu description outside of `p` tag

--- src/components/groups/skin/Layout/LayoutGroup.tsx
+++ src/components/groups/skin/Layout/LayoutGroup.tsx
@@ -10,44 +10,44 @@ export default function LayoutGroup() {
         <Group title="Song Selection Menu Buttons" collapsible>
             <p className="pb-2">
                 There are the same options for each button, namely:
-                <br />
-                <ul>
-                    <li>
-                        <b>Width</b>
-                        <br />
-                        Specifies the width of the button, in pixels. Use -1 to
-                        use the width of the button&apos;s texture.
-                    </li>
-                    <br />
-                    <li>
-                        <b>Height</b>
-                        <br />
-                        Specifies the height of the button, in pixels. Use -1 to
-                        use the height of the button&apos;s texture.
-                    </li>
-                    <br />
-                    <li>
-                        <b>Scale</b>
-                        <br />
-                        Increasing this number increases the size of the button,
-                        and vice versa.
-                    </li>
-                    <br />
-                    <li>
-                        <b>Horizontal Offset</b>
-                        <br />
-                        The horizontal offset of the button from the left of the
-                        screen, in pixels.
-                    </li>
-                    <br />
-                    <li>
-                        <b>Vertical Offset</b>
-                        <br />
-                        The vertical offset of the button from the bottom of the
-                        screen, in pixels.
-                    </li>
-                </ul>
             </p>
+
+            <ul>
+                <li>
+                    <b>Width</b>
+                    <br />
+                    Specifies the width of the button, in pixels. Use -1 to use
+                    the width of the button&apos;s texture.
+                </li>
+                <br />
+                <li>
+                    <b>Height</b>
+                    <br />
+                    Specifies the height of the button, in pixels. Use -1 to use
+                    the height of the button&apos;s texture.
+                </li>
+                <br />
+                <li>
+                    <b>Scale</b>
+                    <br />
+                    Increasing this number increases the size of the button, and
+                    vice versa.
+                </li>
+                <br />
+                <li>
+                    <b>Horizontal Offset</b>
+                    <br />
+                    The horizontal offset of the button from the left of the
+                    screen, in pixels.
+                </li>
+                <br />
+                <li>
+                    <b>Vertical Offset</b>
+                    <br />
+                    The vertical offset of the button from the bottom of the
+                    screen, in pixels.
+                </li>
+            </ul>
 
             <SubGroup title="Back Button" withEditorContainer>
                 <LayoutBackButton />

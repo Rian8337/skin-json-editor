bd5bd4234cbc3e98574153028904b4666ee96066
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-03-10 06:20:47 +0700

    Put header text in center

--- src/components/groups/main/HeaderGroup.tsx
+++ src/components/groups/main/HeaderGroup.tsx
@@ -1,9 +1,12 @@
 export default function HeaderGroup() {
     return (
         <section className="hero">
-            <div className="hero-body">
+            <div className="hero-body has-text-centered">
                 <h1 className="title">osu!droid skin.json editor</h1>
-                <h2 className="subtitle">A tool for editing skin.json files that are used in osu!droid skins.</h2>
+                <h2 className="subtitle">
+                    A tool for editing skin.json files that are used in
+                    osu!droid skins.
+                </h2>
             </div>
         </section>
     );
